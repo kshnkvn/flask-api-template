c36186262bec3d02c79dbe5d06a791b66a901bfc
Ivan Yakushenko <kshnkvn@gmail.com>
2021-02-18 00:30:52 +0200

    visitor model

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String
+
+from app import db
+
+
+class Visitor(db.Model):
+    '''Database for storing site visitors.'''
+
+    __tablename__ = 'Visitors'
+
+    id = Column(Integer, primary_key=True)
+    ip = Column(String, nullable=False, index=True)
+    user_agent = Column(String, nullable=False)
+
+    @classmethod
+    def get_by_ip(cls, ip):
+        return db.session.query(cls).filter(cls.ip==ip).first()
+
+    def __repr__(self):
+        return f'Visitor(id={self.id} ip={self.ip})'
