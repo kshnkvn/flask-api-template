243fd09e51e611a7b4d3cdf6b745f5984ccabc09
Ivan Yakushenko <kshnkvn@gmail.com>
2021-02-18 00:31:08 +0200

    return visitor info

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,34 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify, current_app
+
+from app import db
+from app.models import Visitor
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
-    '''Index route.'''
+    '''Index route.
+    
+    Returns the basic visitor info.
+    '''
 
-    return jsonify({'status': True})
+    ip = request.remote_addr
+
+    visitor = Visitor.get_by_ip(ip)
+    if not visitor:
+        visitor = Visitor(
+            ip=ip,
+            user_agent=request.headers.get('User-Agent')
+        )
+
+        db.session.add(visitor)
+        db.session.commit()
+
+        current_app.logger.info(f'Added new visitor: <{visitor}>')
+
+    return jsonify({
+        'status': True,
+        'ip': visitor.ip,
+        'user-agent': visitor.user_agent
+    })
